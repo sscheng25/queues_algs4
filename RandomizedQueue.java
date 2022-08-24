de511dedeb5691693da50bb61e553711d5aa322d
Sisun Cheng <997723678@qq.com>
2022-08-24 00:23:59 -0400

    Update RandomizedQueue.java

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -38,7 +38,32 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public Item sample()
 
     // return an independent iterator over items in random order
-    public Iterator<Item> iterator()
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException();
+        }
+    }
 
     // unit testing (required)
     public static void main(String[] args) {
